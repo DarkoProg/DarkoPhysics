da84134414610178674a7d4150abbaac2b399940
DarkoProg <marko.golob99@gmail.com>
2024-06-30 17:36:51 +0200

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,40 @@
+#include <glm/ext/vector_float2.hpp>
+#include <glm/glm.hpp>
 #include <glm/vec2.hpp>
 #include <iostream>
+#include <ostream>
 
-struct AABB
-{
+struct Box {
   glm::vec2 min;
   glm::vec2 max;
 };
 
-bool
-isIn (AABB &box1, AABB &box2)
-{
-  if (box1.min.x >= box2.min.x && box1.min.x <= box2.max.x)
-    {
-      if (box1.min.y >= box2.min.y && box1.min.y <= box2.max.y)
-        {
-          return true;
-        }
-    }
+struct Circle {
+  float radius;
+  glm::vec2 position;
+};
+
+bool boxIntersect(Box &a, Box &b) {
+  if (a.max.x < b.min.x or a.min.x > b.max.x) return false;
+  if (a.max.y < b.min.y or a.min.y > b.max.y) return false;
+  return true;
+}
+
+bool circleIntersect(Circle &a, Circle &b) {
+  if (a.radius + b.radius > glm::distance(a.position, b.position)) {
+    return true;
+  }
   return false;
 }
 
-bool
-boxIntersect (AABB &box1, AABB &box2)
-{
-  if (isIn (box1, box2))
-    {
-      return true;
-    }
-  if (isIn (box2, box1))
-    {
-
-      return true;
-    }
-  return false;
-}
-
-int
-main ()
-{
-  char a[10];
-  AABB box1 = AABB{ glm::vec2{ 0, 0 }, glm::vec2{ 1, 1 } };
-  AABB box2 = AABB{ glm::vec2{ 0.5, 0.5 }, glm::vec2{ 1.5, 1.5 } };
-  AABB box3 = AABB{ glm::vec2{ 1.5, 1.5 }, glm::vec2{ 2.5, 2.5 } };
-  AABB box4 = AABB{ glm::vec2{ -1.5, -1.5 }, glm::vec2{ 0.5, 0.5 } };
-  std::cout << boxIntersect (box1, box2);
+int main() {
+  Box box1 = Box{glm::vec2{0, 0}, glm::vec2{1, 1}};
+  Box box2 = Box{glm::vec2{0.5, 0.5}, glm::vec2{1.5, 1.5}};
+  Box box3 = Box{glm::vec2{1.5, 1.5}, glm::vec2{2.5, 2.5}};
+  Box box4 = Box{glm::vec2{-1.5, -1.5}, glm::vec2{0.5, 0.5}};
+  std::cout << boxIntersect(box1, box3) << std::endl;
+  Circle circle1 = Circle{1.0f, glm::vec2{0, 0}};
+  Circle circle2 = Circle{1.0f, glm::vec2{2, 2}};
+  Circle circle3 = Circle{1.0f, glm::vec2{0.5, 0.5}};
+  std::cout << circleIntersect(circle1, circle2);
 }
